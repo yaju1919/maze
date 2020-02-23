f1f6a721e037438cfeb596c6990b8223dc4b58c7
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-02-23 19:58:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
         value: 49,
         save: "w",
         change: function(n){
-            if(height) showExpect(n - 1, height());
+            setTimeout(showExpect);
             if(!(n % 2)) return n - 1;
         }
     });
@@ -29,7 +29,7 @@
         value: 49,
         save: "h",
         change: function(n){
-            showExpect(width(), n - 1);
+            setTimeout(showExpect);
             if(!(n % 2)) return n - 1;
         }
     });
@@ -266,15 +266,17 @@
         int: true,
         save: "rate",
         change: function(){
-            showExpect(width(),height());
+            setTimeout(showExpect);
         }
     });
     var expect = $("<div>").appendTo(ui);
-    function showExpect(w,h){
-        if(!rate) return;
+    function showExpect(){
+        if(!expect) return;
+        var w = width(),
+            h = height();
         var w2 = (w - 1) / 2 * rate() + (w - 1) / 2 + 1,
             h2 = (h - 1) / 2 * rate() + (h - 1) / 2 + 1;
-        if(expect) expect.text("拡大後の幅:" + w2 + ", 高さ:" + h2);
+        expect.text("拡大後の幅:" + w2 + ", 高さ:" + h2);
     }
     $("#rate").trigger("change");
     addBtn("迷路の通路を拡大",expansion);
